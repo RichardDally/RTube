6b3731749a8149dd45e9cebca653e2f0787b96d0
Richard Dally <r.dally@protonmail.com>
2024-11-09 12:43:34 +0100

    Rework setup.py

--- setup.py
+++ setup.py
@@ -17,13 +17,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
-    author_email="r.dally@pm.me",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.7',
+    python_requires='>=3.11',
 )
